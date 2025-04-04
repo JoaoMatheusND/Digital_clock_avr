db1b32327a2d166e15644dbe24236166389ea78f
João Matheus N. Dias <joao.dias@edge.ufal.br>
2025-04-04 14:51:00 -0300

    ADD function to enable the buzzer and delay

--- Digital_clock/main.asm
+++ Digital_clock/main.asm
@@ -37,10 +37,14 @@ jmp RESET ;Interruption PCINT0 - button reset
 .def display_crono = r18 ; Exclusive use for mode 2 (show local cronometro)
 
 ;.SET
+.set hour  = r19
+.set crono = r20
+.set delay = r21
 
 ;.EQU
-.equ full_input  = 0x00 ; If used all pins of a port will be set like input
-.equ full_output = 0xff ; If used all pins of a port will be set like output
+.equ full_input   = 0x00 ; If used all pins of a port will be set like input
+.equ full_output  = 0xff ; If used all pins of a port will be set like output
+.equ clear		  = 0x00 ; Used to reset hour or crono 
 .equ display1	  = 0b00000100 ; Used to power the transistor that will set the unidade dos segundos
 .equ display2	  = 0b00001000 ; Used to power the transistor that will set the dezena dos segundos
 .equ display3	  = 0b00010000 ; Used to power the transistor that will set the unidade dos minutos
@@ -57,6 +61,8 @@ jmp RESET ;Interruption PCINT0 - button reset
 .equ reset_button = 0b00010000
 .equ buttons	  = modo_button | start_button | reset_button
 
+.equ cloclkMHz = 16
+
 
 ;Storages
 show_display: .db display1, display2, display3, display4
@@ -67,6 +73,10 @@ show_display: .db display1, display2, display3, display4
 .org 0x34
 
 INIT:
+	;Initial vlaues
+		ldi r19, 0x00
+		ldi r20, 0x00
+
 	;Stack initialization
 		ldi temp, low(RAMEND)
 		out SPL, temp
@@ -95,10 +105,37 @@ INIT:
 		ldi temp, buttons
 		out DDRD, temp  ; Port D is to receiver the buttons and call the callbacks to system work very well
 
-	;Enable especific interrup
+	;Enable especific interrupt
 
-						
 
+	sei
+
+MAIN:
+
+
+DELAY: ;Recebe o argumento da quantidade de delay em ms por r25
+	ldi r26 , byte3(cloclkMHz * 1000 * r25 / 5)
+	ldi r27, HIGH(byte3(cloclkMHz * 1000 * r25 / 5))
+	ldi r28, LOW(byte3(cloclkMHz * 1000 * r25 / 5))
+
+	sub	r28, 1
+	sbci r27, 0
+	sbci r26, 0
+	brcc pc-3
+	
+	ret 
+
+FUNC_BUZZER:
+	cli ; critical secction - Desanable the global interrupt 
+		ldi temp, buzzer
+		out PORTB temp ; Estar errado por afeta os demais pinos
+
+		ldi r25, 100 ; Await for 250ms until desanable the buzzefr
+
+		ldi temp, buzzer & 0x00 ; Estar errado
+		out PORTB temp
+	sei ; Enable the global interrupt again
+	ret
 
 
 
