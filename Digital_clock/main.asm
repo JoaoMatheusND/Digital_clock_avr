77333bf5af1a2b4d079f97941aa1ee1680ba851a
João Matheus N. Dias <joao.dias@edge.ufal.br>
2025-04-04 17:05:35 -0300

    DOC: add todo's

--- Digital_clock/main.asm
+++ Digital_clock/main.asm
@@ -5,9 +5,9 @@
 *	Copyright (C) [2025] [Digital clock]
 *	
 *	Developed by:
-*	-	Arturo Jiménez Loaiza 
+*	-	Arturo Jimï¿½nez Loaiza 
 *	-	Gabriel Vitorino de Andrade  
-*	-	João Matheus Nascimento Dias 
+*	-	Joï¿½o Matheus Nascimento Dias 
 *	-	Manoel Pedro de Aranda Terceiro
 */
 
@@ -35,6 +35,7 @@ jmp RESET ;Interruption PCINT0 - button reset
 .def temp          = r16 ; Used only with temporary data
 .def display_hour  = r17 ; Exclusive use for modes 1 and 3 (show and adjust time for display)
 .def display_crono = r18 ; Exclusive use for mode 2 (show local cronometro)
+.def stack		   = r0  ; Exclusive use for stack SREG config
 
 ;.SET
 .set hour  = r19
@@ -78,10 +79,10 @@ INIT:
 		ldi r20, 0x00
 
 	;Stack initialization
-		ldi temp, low(RAMEND)
-		out SPL, temp
-		ldi temp, high(RAMEND)
-		out SPH, temp
+		ldi stack, low(RAMEND)
+		out SPL, stack
+		ldi stack, high(RAMEND)
+		out SPH, stack
 
 	;Config timer to modo1
 		#define CLOCK 16.0e6 ;clock speed
@@ -114,9 +115,9 @@ MAIN:
 
 
 DELAY: ;Recebe o argumento da quantidade de delay em ms por r25
-	ldi r26 , byte3(cloclkMHz * 1000 * r25 / 5)
-	ldi r27, HIGH(byte3(cloclkMHz * 1000 * r25 / 5))
-	ldi r28, LOW(byte3(cloclkMHz * 1000 * r25 / 5))
+	ldi r26 , byte3(cloclkMHz * 1000 * temp / 5)
+	ldi r27, HIGH(byte3(cloclkMHz * 1000 * temp / 5))
+	ldi r28, LOW(byte3(cloclkMHz * 1000 * temp / 5))
 
 	sub	r28, 1
 	sbci r27, 0
@@ -126,16 +127,50 @@ DELAY: ;Recebe o argumento da quantidade de delay em ms por r25
 	ret 
 
 FUNC_BUZZER:
-	cli ; critical secction - Desanable the global interrupt 
-		ldi temp, buzzer
-		out PORTB temp ; Estar errado por afeta os demais pinos
+    cli ; Critical section - turn the global interruptcion off 
 
-		ldi r25, 100 ; Await for 250ms until desanable the buzzefr
+    sbi PORTB, buzzer  ; Turn buzzer on
 
-		ldi temp, buzzer & 0x00 ; Estar errado
-		out PORTB temp
-	sei ; Enable the global interrupt again
-	ret
+    ldi temp, 250       ; Await 250ms 
+	rcall DELAY
 
+    cbi PORTB, buzzer  ; Turn buzzer off
 
+    sei ; Active the global interruptcion 
+    ret
 
+	MODO:
+	push stack
+	in stack, SREG
+	push stack
+
+	;TO DO: Implementar a lógica de troca de estado e as chamdas de rotinas necessárias para garante a execução ideal do próximo estadó
+
+	pop stack
+	out SREG, stack
+	pop stack
+	reti
+
+	START:
+	push stack
+	in stack, SREG
+	push stack
+
+	;TO DO: Muita coisa
+
+	pop stack
+	out SREG, stack
+	pop stack
+	reti
+
+	RESET:
+	push stack
+	in stack, SREG
+	push stack
+
+	;TO DO: Muita coisa
+
+	pop stack
+	out SREG, stack
+	pop stack
+	reti
